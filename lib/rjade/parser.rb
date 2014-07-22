a57b84c4c6d031b8294228dc81826bd44b8d0bb9
Roman Kříž <samnung@gmail.com>
2014-07-22 18:12:48 +0200

    [Parser] added method for parsing complex ruby code (brackets, quotes, ...)

--- lib/rjade/parser.rb
+++ lib/rjade/parser.rb
@@ -487,6 +487,64 @@ module RJade
 			end
 		end
 
+		# Parse ruby code, ended with outer delimiters
+		# @param [String] outer_delimiters
+		#
+		# @return [String] parsed ruby code
+		#
+		def parse_ruby_code(outer_delimiters)
+			code = ''
+			end_re = /\A\s*[#{Regexp.escape outer_delimiters.to_s}]/
+			delimiters = []
+
+			until @line.empty? or (delimiters.count == 0 and @line =~ end_re)
+				char = @line[0]
+
+				# backslash escaped delimiter
+				if char == '\\' && RUBY_ALL_DELIMITERS.include?(@line[1])
+					code << @line.slice!(0, 2)
+					next
+				end
+
+				case char
+					when RUBY_START_DELIMITERS_RE
+						if RUBY_NOT_NESTABLE_DELIMITERS.include?(char) && delimiters.last == char
+							delimiters.pop
+						else
+							delimiters << char
+						end
+
+					when RUBY_END_DELIMITERS_RE
+						if char == RUBY_DELIMITERS_REVERSE[delimiters.last]
+							delimiters.pop
+						end
+				end
+
+				code << @line.slice!(0)
+			end
+
+			unless delimiters.empty?
+				syntax_error('Unexpected end of ruby code')
+			end
+
+			code.strip
+		end
+
+		RUBY_DELIMITERS_REVERSE = {
+			'(' => ')',
+			'[' => ']',
+			'{' => '}'
+		}.freeze
+
+		RUBY_NOT_NESTABLE_DELIMITERS = %w(' " |).freeze
+
+		RUBY_START_DELIMITERS = %w(\( [ { | ' ").freeze
+		RUBY_END_DELIMITERS = %w(\) ] } | ' ").freeze
+		RUBY_ALL_DELIMITERS = RUBY_START_DELIMITERS + RUBY_END_DELIMITERS
+
+		RUBY_START_DELIMITERS_RE = /\A[#{Regexp.escape RUBY_START_DELIMITERS.join('')}]/
+		RUBY_END_DELIMITERS_RE = /\A[#{Regexp.escape RUBY_END_DELIMITERS.join('')}]/
+
 
 		# ----------- Errors ---------------
 
--- spec/parser/core_spec.rb
+++ spec/parser/core_spec.rb
@@ -68,5 +68,72 @@ module RJade
 				assert_remove 4, "\t\tabc", 'abc', :tabsize => 2
 			end
 		end
+
+
+
+		context '#parse_ruby_code' do
+			def assert_ruby_code(source, expected_ruby, end_delimiter = ',)', options = {})
+				sut = Parser.new(options)
+
+				sut.instance_eval { @lines = [source] }
+				sut.next_line
+
+				result_ruby = sut.parse_ruby_code end_delimiter
+
+				expect(result_ruby).to eq expected_ruby
+			end
+
+			it 'parse string' do
+				assert_ruby_code ' "abc" ,', '"abc"'
+			end
+			it 'parse string with colons' do
+				assert_ruby_code ' "abc ,fd ,, fds f" ,   ', '"abc ,fd ,, fds f"'
+			end
+			it 'parse string with mixed quotes' do
+				source = %Q{ "text with \\', \\" quotes" 'other, text ' }
+				expected = %Q{"text with \\', \\" quotes" 'other, text '}
+				assert_ruby_code source, expected
+			end
+
+			it 'should not parse uncomplete text' do
+				source = ' some_method(param   , '
+
+				expect {
+					assert_ruby_code source, nil
+				}.to raise_error
+			end
+
+			it 'parse simple variable' do
+				assert_ruby_code 'abc,', 'abc'
+			end
+			it 'parse simple variable with calling method' do
+				assert_ruby_code 'abc.lowercase,', 'abc.lowercase'
+			end
+
+			it 'parse calling function with parameter' do
+				assert_ruby_code 'function(abc)   ,', 'function(abc)'
+			end
+			it 'parse calling function with more parameters' do
+				assert_ruby_code 'function(abc, 123)    ,', 'function(abc, 123)'
+			end
+
+			it 'parsed value is striped' do
+				assert_ruby_code '    some code     ', 'some code'
+			end
+
+			it 'parse accessing with square brackets' do
+				assert_ruby_code ' hash[text]   ,', 'hash[text]'
+			end
+			it 'parse accessing with square brackets with string' do
+				assert_ruby_code ' hash["text"]   ,', 'hash["text"]'
+			end
+			it 'parse accessing with square brackets with multiple parameters' do
+				assert_ruby_code ' hash[text,text2]   ,', 'hash[text,text2]'
+			end
+
+			it 'parse blocks' do
+				assert_ruby_code '  hash.map { |baf| baf, baf }  ', 'hash.map { |baf| baf, baf }'
+			end
+		end
 	end
 end
