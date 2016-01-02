30ae8494a886f1805c3e73ccf354c1bc37b21875
Roman Kříž <samnung@gmail.com>
2016-01-02 22:14:06 +0100

    [Runtime::Mixin] fix exception message parsing for older MRI versions
    refs #8

--- lib/bade/runtime/mixin.rb
+++ lib/bade/runtime/mixin.rb
@@ -8,17 +8,20 @@ module Bade
       def call!(*args)
         block.call(*args)
       rescue ArgumentError => e
-        if e.message =~ /\Awrong number of arguments \(given ([0-9]+), expected ([0-9]+)\)\Z/
-          # handle incorrect parameters
+        case e.message
+        when /\Awrong number of arguments \(given ([0-9]+), expected ([0-9]+)\)\Z/, /\Awrong number of arguments \(([0-9]+) for ([0-9]+)\)\Z/
+          # handle incorrect parameters count
 
           # minus one, because first argument is always hash of blocks
           given = $1.to_i - 1
           expected = $2.to_i - 1
           raise ArgumentError, "wrong number of arguments (given #{given}, expected #{expected}) for mixin `#{name}`"
-        elsif e.message =~ /\Aunknown keyword: (.*)\Z/
+
+        when /\Aunknown keyword: (.*)\Z/
           # handle unknown key-value parameter
           key_name = $1
           raise ArgumentError, "unknown key-value argument `#{key_name}` for mixin `#{name}`"
+
         else
           raise
         end
