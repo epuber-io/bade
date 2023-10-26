4107e568f700f56bf1c204398f54ced11ef523f3
Roman Kříž <roman@kriz.io>
2023-10-26 16:03:23 +0200

    fix(generator): dynamic values in require_relative

--- spec/renderer/import_real_spec.rb
+++ spec/renderer/import_real_spec.rb
@@ -46,4 +46,33 @@ describe Bade::Renderer, 'import feature (real files)' do
 
     expect(output).to eq '<div>123</div>'
   end
+
+  it 'does not brake when dynamic value in string' do
+    write_file.call('epuber_common/lib/utils.rb', <<~RUBY)
+      def abc
+        '123'
+      end
+    RUBY
+
+    write_file.call('text/__base.bade', <<~BADE)
+      - require_relative "\#{File.dirname(current_file_path)}/../epuber_common/lib/utils.rb"
+
+      mixin cover_page
+        div= abc
+    BADE
+
+    write_file.call('text/copyright.bade', <<~BADE)
+      import '__base.bade'
+
+      +cover_page
+    BADE
+
+    output = Bade::Renderer.from_file(File.join(working_dir, 'text/copyright.bade'))
+                           .with_locals(
+                              current_file_path: File.join(working_dir, 'text/copyright.bade'),
+                            )
+                           .render(new_line: '')
+
+    expect(output).to eq '<div>123</div>'
+  end
 end
